f8ba1f3633b3c1447f3cd2ef8b05464f58cd973a
ottojoaopaulo <joao.otto.11@gmail.com>
2023-06-09 10:07:43 -0300

    versao3

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 def cadastro (request):
     if request.method == "GET":  
@@ -22,4 +25,22 @@ def cadastro (request):
         return HttpResponse('Usuario cadastrado com sucesso')
 
 def login (request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+            return HttpResponse('autenticado')
+        else: 
+            return HttpResponse('Email ou senha invalido')
+
+@login_required(login_url='/auth/login/')
+def plataforma (request):
+    # if request.user.is_authenticated:
+    #     return HttpResponse('Plataforma')
+    return HttpResponse('voce esta na plataforma')
